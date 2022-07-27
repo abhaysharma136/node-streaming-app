93ee994645ed5d6be4f2b0922a743912e9c9ecef
Abhay Sharma <abhaysharmajr@gmail.com>
2022-07-27 15:33:11 +0530

    install nodemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
 app.get("/",function(request,responce){
-    responce.send("Hello World");
+    responce.send("Welcome to Onstream");
 });
 app.listen(3000);
