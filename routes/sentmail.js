7dd3bbaee727cd0f0d1deea5853e00370251025e
Abhay Sharma <abhaysharmajr@gmail.com>
2022-10-06 09:55:56 +0530

    user

--- routes/sentmail.js
+++ routes/sentmail.js
@@ -1,107 +1,111 @@
-import express from 'express';
-import  jwt  from 'jsonwebtoken';
-import nodemailer from 'nodemailer';
+import express from "express";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
+const router = express.Router();
 
-const router=express.Router();
+export async function sentEmail(receiver) {
+  var receiver = receiver;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "abhaysharmajr@gmail.com",
+      pass: process.env.PASS,
+    },
+  });
 
-   export async function sentEmail(receiver){
-    var receiver=receiver;
-    var transporter=nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:'abhaysharmajr@gmail.com',
-            pass:process.env.PASS
-        }
-    });
-    
-    const token=jwt.sign({
-        data:'Token Data'
-    }, 'ourSecretKey',{expiresIn:'10m'}
-    );
+  const token = jwt.sign(
+    {
+      data: "Token Data",
+    },
+    "ourSecretKey",
+    { expiresIn: "10m" }
+  );
 
+  var mailOptions = {
+    from: "abhaysharmajr@gmail.com",
+    to: receiver,
+    subject: "password change Email with Link",
+    text: `Hi! There, You Forgot your password? We received a request to reset your password. Click on the link or Copy paste the URl in your browser.   http://prismatic-syrniki-2f6692.netlify.app/Onstream/forgotPassword/${receiver}/${token} The provided link will expire in 10 minutes. Thanks`,
+    // html:'<p> You requested for reset password, kindly use this <a href="http://localhost:4000/reset-password?token=' + '">link</a> to reset your password</p>'
+  };
 
-    var mailOptions={
-        from:'abhaysharmajr@gmail.com',
-        to:receiver,
-        subject:'password change Email with Link',
-        text:`Hi! There, You Forgot your password? We received a request to reset your password. Click on the link or Copy paste the URl in your browser.   http://prismatic-syrniki-2f6692.netlify.app/Onstream/forgotPassword/${receiver}/${token} The provided link will expire in 10 minutes. Thanks`,
-        // html:'<p> You requested for reset password, kindly use this <a href="http://localhost:4000/reset-password?token=' + '">link</a> to reset your password</p>'
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
-    
-    transporter.sendMail(mailOptions,function(error,info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Email sent: '+ info.response);
-        }
-    })
+  });
 }
-router.post("/sent",async function(request,response){
-    const data=request.body.email;
-    console.log(data);
-    const result=await sentEmail(data);
+router.post("/sent", async function (request, response) {
+  const data = request.body.email;
+  console.log(data);
+  const result = await sentEmail(data);
 
-    response.send(result);
+  response.send(result);
 });
-
 
 //Sent Registration Email
-export async function sentRegistrationEmail(receiver){
-    var receiver=receiver;
-    var transporter=nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:'abhaysharmajr@gmail.com',
-            pass:process.env.PASS
-        }
-    });
-    
-    const token=jwt.sign({
-        data:'Token Data'
-    }, 'ourSecretKey',{expiresIn:'10m'}
-    );
+export async function sentRegistrationEmail(receiver) {
+  var receiver = receiver;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "abhaysharmajr@gmail.com",
+      pass: process.env.PASS,
+    },
+  });
 
+  const token = jwt.sign(
+    {
+      data: "Token Data",
+    },
+    "ourSecretKey",
+    { expiresIn: "10m" }
+  );
 
-    var mailOptions={
-        from:'abhaysharmajr@gmail.com',
-        to:receiver,
-        subject:'Onstream Registration Confirmation',
-        text:`Hi! There, You have recently visited our website and entered your email. Please follow the given link to verify your email below.   http://prismatic-syrniki-2f6692.netlify.app/Onstream/AccountConfirmation/${receiver}/${token} Thanks`,
-        // html:"<p><a href=http://localhost:5000/email/verify/${token}></a></p>"
+  var mailOptions = {
+    from: "abhaysharmajr@gmail.com",
+    to: receiver,
+    subject: "Onstream Registration Confirmation",
+    text: `Hi! There, You have recently visited our website and entered your email. Please follow the given link to verify your email below.   http://prismatic-syrniki-2f6692.netlify.app/Onstream/AccountConfirmation/${receiver}/${token} Thanks`,
+    // html:"<p><a href=http://localhost:5000/email/verify/${token}></a></p>"
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
-    
-    transporter.sendMail(mailOptions,function(error,info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Email sent: '+ info.response);
-        }
-    })
+  });
 }
 
-router.post("/RegisterConfirmation",async function(request,response){
-    const data=request.body.email;
-    console.log(data);
-    const result=await sentRegistrationEmail(data);
+router.post("/RegisterConfirmation", async function (request, response) {
+  const data = request.body.email;
+  console.log(data);
+  const result = await sentRegistrationEmail(data);
 
-    response.send(result);
+  response.send(result);
 });
 
-router.get('/verify/:token',async function(request,response){
-    const {token}=request.params;
+router.get("/verify/:token", async function (request, response) {
+  const { token } = request.params;
 
-    //verifying the JWT token
-    jwt.verify(token,'ourSecretKey',function(err,decoded){
-        if(err){
-            console.log(err);
-            response.send({message:"Email verification failed, possibly the link is invalid or expired"});
-        }
-        else{
-            console.log("email succesfully Registered")
-            response.send({message:"Email verified successfully"});
-        }
-    });
+  //verifying the JWT token
+  jwt.verify(token, "ourSecretKey", function (err, decoded) {
+    if (err) {
+      console.log(err);
+      response.send({
+        message:
+          "Email verification failed, possibly the link is invalid or expired",
+      });
+    } else {
+      console.log("email succesfully Registered");
+      response.send({ message: "Email verified successfully" });
+    }
+  });
 });
 
-export const emailRouter=router;
+export const emailRouter = router;
--- routes/users.js
+++ routes/users.js
@@ -1,233 +1,239 @@
-import express from 'express';
-import { CreateUser, DeleteuserById, GetAllUsers, GetUserById, getUserByname, UpdateUserByEmail, UpdateUserById } from './helper.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { Client } from '../index.js  ';
-import { auth } from '../middleware/auth.js';
-const router=express.Router();
+import express from "express";
+import {
+  CreateUser,
+  DeleteuserById,
+  GetAllUsers,
+  GetUserById,
+  getUserByname,
+  UpdateUserByEmail,
+  UpdateUserById,
+} from "./helper.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Client } from "../index.js  ";
+import { auth } from "../middleware/auth.js";
+const router = express.Router();
 
-
-async function genHashedpassword(password){
-    const NO_OF_ROUND=10;
-    const salt=await bcrypt.genSalt(NO_OF_ROUND);
-    const hashedPassord=await bcrypt.hash(password,salt);
-    console.log(salt,hashedPassord);
-    return hashedPassord;
+async function genHashedpassword(password) {
+  const NO_OF_ROUND = 10;
+  const salt = await bcrypt.genSalt(NO_OF_ROUND);
+  const hashedPassord = await bcrypt.hash(password, salt);
+  console.log(salt, hashedPassord);
+  return hashedPassord;
 }
 
-
 //GET ALL User
-router.get("/",async function(request,response){
-    //Get movie with name,rating
-    // if(request.query.rating){
-    //     request.query.rating=+request.query.rating;
-    // }
-    console.log(request.query)
-    const users=await GetAllUsers(request);
-    // console.log(movies);
-    response.send(users);
+router.get("/", async function (request, response) {
+  //Get movie with name,rating
+  // if(request.query.rating){
+  //     request.query.rating=+request.query.rating;
+  // }
+  console.log(request.query);
+  const users = await GetAllUsers(request);
+  // console.log(movies);
+  response.send(users);
 });
 
+//Create User
+router.post("/signup", async function (request, response) {
+  const { email, password, FirstName, LastName } = request.body;
 
-//Create User 
-router.post("/signup", async function(request,response){
-    const {email,password,FirstName,LastName}=request.body;
+  const UserFromDB = await getUserByname(email);
+  console.log(UserFromDB);
+  if (UserFromDB) {
+    response.status(400).send({ message: "email allready exists" });
+  } else if (password.length < 8) {
+    response.send({ message: "password must be atleast 8 characters" });
+  } else {
+    const hashedPassword = await genHashedpassword(password);
+    console.log(hashedPassword);
+    //db.movies.insertOne(data)
+    const result = await CreateUser({
+      email: email,
+      password: hashedPassword,
+      FirstName: FirstName,
+      LastName: LastName,
+      confirm: false,
+    });
+    response.send({ result, email, message: "Email Sent to registered Email" });
+  }
+});
 
-    const UserFromDB=await getUserByname(email);
-    console.log(UserFromDB);
-    if(UserFromDB){
-        response.status(400).send({message:"email allready exists"});
-    }else if(password.length<8){
-        response.send({message:"password must be atleast 8 characters"});
-    }else{const hashedPassword=await genHashedpassword(password);
-        console.log(hashedPassword);
-        //db.movies.insertOne(data)
-        const result=await CreateUser({
-            email:email,
-            password:hashedPassword,
-            FirstName:FirstName,
-            LastName:LastName,
-            confirm:false
+router.post("/login", async function (request, response) {
+  const { email, password } = request.body;
+
+  const UserFromDB = await getUserByname(email);
+  console.log(UserFromDB);
+  if (!UserFromDB) {
+    response.status(400).send({ message: "email does not match" });
+  } else if (UserFromDB.confirm === false) {
+    response.status(400).send({ message: "Account not Verified" });
+  } else {
+    const storedpassword = UserFromDB.password;
+    const isPasswordMatch = await bcrypt.compare(password, storedpassword);
+    console.log(isPasswordMatch);
+    if (email === "abhaysharmajr@gmail.com") {
+      if (isPasswordMatch) {
+        const token = jwt.sign({ id: UserFromDB._id }, process.env.SECRET_KEY);
+        response.send({
+          message: "Succesfull Admin Login",
+          token: token,
+          id: UserFromDB._id,
         });
-        response.send({result,message:"Email Sent to registered Email"});
+      } else {
+        response.status(401).send({ message: "Invalid Credentials" });
+      }
+    } else {
+      if (isPasswordMatch) {
+        const token = jwt.sign({ id: UserFromDB._id }, process.env.SECRET_KEY);
+        response.send({
+          message: "Succesfull Login",
+          token: token,
+          id: UserFromDB._id,
+        });
+      } else {
+        response.status(401).send({ message: "Invalid Credentials" });
+      }
     }
-    
-});
-
-router.post("/login", async function(request,response){
-    const {email,password}=request.body;
-
-    const UserFromDB=await getUserByname(email);
-    console.log(UserFromDB);
-    if(!UserFromDB){
-        response.status(400).send({message:"email does not match"});
-    }else if(UserFromDB.confirm===false){
-        response.status(400).send({message:"Account not Verified"})
-    }else
-    {
-       const storedpassword=UserFromDB.password;
-       const isPasswordMatch=await bcrypt.compare(password,storedpassword);
-       console.log(isPasswordMatch);
-       if(email==="abhaysharmajr@gmail.com"){
-        if(isPasswordMatch){
-            const token=jwt.sign({id:UserFromDB._id},process.env.SECRET_KEY);
-            response.send({message:"Succesfull Admin Login",token:token,id:UserFromDB._id});
-           }
-           else{
-            response.status(401).send({message:"Invalid Credentials"});
-           }
-       }
-       else{
-        if(isPasswordMatch){
-            const token=jwt.sign({id:UserFromDB._id},process.env.SECRET_KEY);
-            response.send({message:"Succesfull Login",token:token,id:UserFromDB._id});
-           }
-           else{
-            response.status(401).send({message:"Invalid Credentials"});
-           }
-       }
-       
-    }
-    
+  }
 });
 //Forgot Password
-router.post("/forgotPassword", async function(request,response){
-    const {email}=request.body;
+router.post("/forgotPassword", async function (request, response) {
+  const { email } = request.body;
 
-    const UserFromDB=await getUserByname(email);
-    console.log(UserFromDB);
-    if(!UserFromDB){
-        response.status(400).send({message:"email not found "});
-    }else if(UserFromDB.confirm===false){
-        response.status(400).send({message:"email is not verified. Please try to verrify first! "});
-    }
-    else
-    {
-        response.status(401).send({message:"email Sent"});  
-    }
-    
+  const UserFromDB = await getUserByname(email);
+  console.log(UserFromDB);
+  if (!UserFromDB) {
+    response.status(400).send({ message: "email not found " });
+  } else if (UserFromDB.confirm === false) {
+    response.status(400).send({
+      message: "email is not verified. Please try to verrify first! ",
+    });
+  } else {
+    response.status(401).send({ message: "email Sent" });
+  }
 });
 
 //Check if provided email Exists, allready verified or notverified
-router.post("/verfyaccountstatus", async function(request,response){
-    const {email}=request.body;
+router.post("/verfyaccountstatus", async function (request, response) {
+  const { email } = request.body;
 
-    const UserFromDB=await getUserByname(email);
-    console.log(UserFromDB);
-    if(!UserFromDB){
-        response.status(400).send({message:"email not found!!. Please register first "});
-    }else if(UserFromDB.confirm===true){
-        response.status(400).send({message:"email is allready verified. Please Login! "});
-    }
-    else{
-        response.status(401).send({message:"email Sent"});  
-    }
-    
+  const UserFromDB = await getUserByname(email);
+  console.log(UserFromDB);
+  if (!UserFromDB) {
+    response
+      .status(400)
+      .send({ message: "email not found!!. Please register first " });
+  } else if (UserFromDB.confirm === true) {
+    response
+      .status(400)
+      .send({ message: "email is allready verified. Please Login! " });
+  } else {
+    response.status(401).send({ email, message: "email Sent" });
+  }
 });
 
-//Update user data from Email 
-router.put("/ConfirmAccount/:email", async function(request,response){
-    const {email}=request.params;
+//Update user data from Email
+router.put("/ConfirmAccount/:email", async function (request, response) {
+  const { email } = request.params;
 
-    console.log(request.params,email);
-    const data=request.body;
-    //db.collection.UpdateOne({id:id},{$set:data})
-    const result=await UpdateUserByEmail(email, data);
-    
-    console.log(result);
+  console.log(request.params, email);
+  const data = request.body;
+  //db.collection.UpdateOne({id:id},{$set:data})
+  const result = await UpdateUserByEmail(email, data);
 
-    response.send(result);
-    
+  console.log(result);
+
+  response.send(result);
 });
 
+//Update user password from Email
+router.put("/updatepassword/:email", async function (request, response) {
+  const { email } = request.params;
 
-//Update user password from Email 
-router.put("/updatepassword/:email", async function(request,response){
-    const {email}=request.params;
+  console.log(request.params, email);
+  const { password } = request.body;
+  //db.collection.UpdateOne({id:id},{$set:data})
+  const hashedPassword = await genHashedpassword(password);
+  const result = await UpdateUserByEmail(email, {
+    password: hashedPassword,
+  });
 
-    console.log(request.params,email);
-    const {password}=request.body;
-    //db.collection.UpdateOne({id:id},{$set:data})
-    const hashedPassword=await genHashedpassword(password);
-    const result=await UpdateUserByEmail(email, {
-        password:hashedPassword
-    });
-    
-    console.log(result);
+  console.log(result);
 
-    response.send(result);
-    
+  response.send(result);
 });
 
 //Get User Data from Id
-router.get("/:id",async function(request,response){
-    const {id}=request.params;
-    console.log(request.params,id);
-    //db.collection.find({id:id})
-    const user=await GetUserById(id);
-    // const movie=movies.find((mv)=>mv.id===id);
-    console.log(user);
+router.get("/:id", async function (request, response) {
+  const { id } = request.params;
+  console.log(request.params, id);
+  //db.collection.find({id:id})
+  const user = await GetUserById(id);
+  // const movie=movies.find((mv)=>mv.id===id);
+  console.log(user);
 
-    user?response.send(user):response.status(404).send({message:"User not Found"});
+  user
+    ? response.send(user)
+    : response.status(404).send({ message: "User not Found" });
 });
 
 //GET USER COUNT
-router.get("/Count/All",auth,async function(request,response){
-    //Get movie with name,rating
-    
-    console.log(request.query)
-    const users=await Client.db("Onstream-db")
-    .collection("users").count(request.query,function(err,result){
-        if(err){
-            response.send(err)
-        }
-        else{
-            response.json(result)
-        }
+router.get("/Count/All", auth, async function (request, response) {
+  //Get movie with name,rating
+
+  console.log(request.query);
+  const users = await Client.db("Onstream-db")
+    .collection("users")
+    .count(request.query, function (err, result) {
+      if (err) {
+        response.send(err);
+      } else {
+        response.json(result);
+      }
     });
-    
-    // response.status(404).sendStatus(movies);
+
+  // response.status(404).sendStatus(movies);
 });
 
 //Update User profile By ID
-router.put("/:id",async function(request,response){
-    const {id}=request.params;
-    console.log(request.params,id);
-    const data=request.body;
-    //db.collection.UpdateOne({id:id},{$set:data})
-    const result=await UpdateUserById(id, data);
-    
-    console.log(result);
+router.put("/:id", async function (request, response) {
+  const { id } = request.params;
+  console.log(request.params, id);
+  const data = request.body;
+  //db.collection.UpdateOne({id:id},{$set:data})
+  const result = await UpdateUserById(id, data);
 
-    response.send(result);
+  console.log(result);
+
+  response.send(result);
 });
 
 //sent Registration email
-router.post("/newuser", async function(request,response){
-    const {email}=request.body;
+router.post("/newuser", async function (request, response) {
+  const { email } = request.body;
 
-    const UserFromDB=await getUserByname(email);
-    console.log(UserFromDB);
-    if(UserFromDB){
-        response.status(400).send({message:"email allready exists"});
-    }else{
-        response.send({message:"Email Sent to registered Email"});
-    }
+  const UserFromDB = await getUserByname(email);
+  console.log(UserFromDB);
+  if (UserFromDB) {
+    response.status(400).send({ message: "email allready exists" });
+  } else {
+    response.send({ message: "Email Sent to registered Email" });
+  }
 });
-
 
 //DELETE User with id
-router.delete("/:id",async function(request,response){
-    const {id}=request.params;
-    console.log(request.params,id);
-    //db.collection.find({id:id})
-    const result=await DeleteuserById(id);
-    // const movie=movies.find((mv)=>mv.id===id);
-    console.log(result);
+router.delete("/:id", async function (request, response) {
+  const { id } = request.params;
+  console.log(request.params, id);
+  //db.collection.find({id:id})
+  const result = await DeleteuserById(id);
+  // const movie=movies.find((mv)=>mv.id===id);
+  console.log(result);
 
-    result.deletedCount>0?response.send({"msg":"User Succesfully Deleted"}):response.status(404).send({"msg":"User not Found"});
+  result.deletedCount > 0
+    ? response.send({ msg: "User Succesfully Deleted" })
+    : response.status(404).send({ msg: "User not Found" });
 });
 
-export const usersRouter=router;
-
-
+export const usersRouter = router;
